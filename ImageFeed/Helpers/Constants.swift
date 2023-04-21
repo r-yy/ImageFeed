c12eb2b0650f7a7d46e6de57048d4bcfd4e20c73
Ramil <r-yy@outlook.com>
2023-04-21 17:44:00 +0300

    Add Unsplash API constants

--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct UnsplashAPI {
+    let accessKey = "doLPUOLi1Ypom-FZmM09AlIKUpPkh6oI-dRFhhgqa_Q"
+    let cecretKey = "ZOKm4tVrTxHuEMXVLX8ooSL-zt3Ru6zdhY_NypoOYBY"
+
+    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+    let acessScope = "public+read_user+write_likes"
+
+    let defaultBaseUrl: URL = {
+        guard let url = URL(string: "https://api.unsplash.com/") else {
+            preconditionFailure("Unsplash API is unavailable")
+        }
+        return url
+    }()
+}
